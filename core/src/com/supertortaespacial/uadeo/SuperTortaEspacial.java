e6e1781741f3b5af971918b918b276b9baea0c97
Wyvern500 <grassyclown@gmail.com>
2022-09-23 19:03:02 -0600

    Test 2

--- core/src/com/supertortaespacial/uadeo/SuperTortaEspacial.java
+++ core/src/com/supertortaespacial/uadeo/SuperTortaEspacial.java
@@ -9,18 +9,14 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SuperTortaEspacial extends ApplicationAdapter {
 	
-	public ShapeRenderer renderer;
-	
 	@Override
 	public void create () {
-		renderer = new ShapeRenderer();
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		renderer.rect(0, 0, 100, 100);
-		renderer.setColor(Color.RED);
 	}
 	
 	@Override
